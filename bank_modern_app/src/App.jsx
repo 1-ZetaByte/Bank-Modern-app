fc4a72443a335076cd101db1f1febacb00b3bd48
kodegeeek <vikas.madd01@gmail.com>
2024-02-23 18:07:21 +0530

    Logo placed
    Solved the rendering issue(all the react components that being rendered, those components must be created , and placed with some boilerplate code rafce , and only then those components must be exported or imported or rendered)

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,35 +1,47 @@
 import React from 'react'
 
-import styles from '/style'
+import styles from './style'
 
+// import Navbar from './components/Navbar'
 
+import {Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero
+} from './components'
 
 const App = () => {
   return (
-    <div className='bg-primary w-full overflow-hidden'>
+    <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexcenter}`}>
         <div className={`${styles.boxwidth}`}>
-          Navbar
+          <Navbar />
         </div>
       </div>
 
 
-      <div  className={`bg-primary ${styles.flexStart}`}>
+      <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxwidth}`}>
-          Hero
+          <Hero />
         </div>
       </div>
 
-      <div  className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxwidth}`}>
-          stats
-          business
-          billing
-          carddeal
-          testimonials
-          clients 
-          cta
-          footer 
+          <Stats />
+          <Business / >
+          <Billing / >
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
         </div>
       </div>
 
